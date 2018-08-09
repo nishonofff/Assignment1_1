345bcf2b24d732e2aed2ed8adf99e4af8409ce3d
nishonofff <34509412+nishonofff@users.noreply.github.com>
2018-08-09 11:13:57 +0500

    CustomSetter for Data binding

---
+++ app/src/main/java/com/example/garik/assignment1/Adapters/CustomSetter.java
@@ -0,0 +1,21 @@
+package com.example.garik.assignment1.Adapters;
+
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by GaRiK on 08.08.2018.
+ */
+
+public class CustomSetter {
+
+    @BindingAdapter("imgSrc")
+    public static void setImgSrc(ImageView view,int resId){
+        view.setImageDrawable((view.getContext().getDrawable(resId)));
+        Picasso.with(view.getContext())
+                .load(resId)
+                .into(view);
+    }
+}
